c78d318cc17f9e329c463cadd3c24e470fb99fc0
filip.sakr <sakrfilip@seznam.cz>
2025-10-24 12:49:06 +0200

    druhejsakr

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Prvni from './Pages/Prvni'
+import Druhy from './Pages/Druhy'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
     <Prvni></Prvni>
+    <Druhy/>
     </>
   )
 }
---
+++ src/Pages/Druhy.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Druhy() {
+  return (
+    <div>Druhy</div>
+  )
+}
